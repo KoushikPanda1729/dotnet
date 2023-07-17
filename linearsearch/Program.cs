ba1f32828b926bbdd8c051bb9dd658f44579fbb4
Git_Koushik <programmer747767@gmail.com>
2023-07-17 22:36:37 +0530

    liner search completed

--- linearsearch/Program.cs
+++ linearsearch/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the size ");
+        int size = int.Parse(Console.ReadLine());
+
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Enter the Elements ");
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter the target ");
+        int searchValue = int.Parse(Console.ReadLine());
+
+
+
+        int index = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == searchValue)
+            {
+                index = i;
+            }
+        }
+        if (index == 0)
+        {
+            Console.WriteLine($"The target {searchValue} is not exist");
+        }
+        else
+        {
+            Console.WriteLine($"The target {searchValue} is found in {index} index ");
+        }
+
+
+    }
+}
